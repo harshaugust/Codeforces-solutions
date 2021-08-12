ae1471e80b4b18ad3556cf003e2c8291d5317827
harshaugust <58336707+harshaugust@users.noreply.github.com>
2021-08-12 16:20:22 +0530

    Create B. Dima and To-do List.cpp

---
+++ B. Dima and To-do List.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <stack>
+#include <iomanip>
+using namespace std;
+
+#define ft first
+#define sd second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ll long long int
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define pii pair<int,int>
+#define plii pair<pair<ll, int>, int>
+#define piii pair<pii, int>
+#define viii vector<pair<pii, int>>
+#define vl vector<ll>
+#define vll vector<pair<ll,ll>>
+#define pll pair<ll,ll>
+#define pli pair<ll,int>
+#define mp make_pair
+#define ms(x, v) memset(x, v, sizeof x)
+const int mod = 1e9 + 7;
+const int maxn = 1e5 + 5;
+const int N=100005;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+    #endif
+    int n, k;
+    cin >> n >> k;
+    vi a(n), s(n);
+    for (auto & x: a) cin >> x;
+
+    for (int i = 0; i < n; i++) {
+        s[i % k] += a[i];
+    }
+    cout << min_element(s.begin(), s.begin() + k) - s.begin() + 1 << endl;
+   return 0;
+}
