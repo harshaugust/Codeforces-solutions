caae61fed6ed75bf7d89be8597e236340f817743
harshaugust <58336707+harshaugust@users.noreply.github.com>
2021-08-12 17:44:54 +0530

    Create B. Han Solo and Lazer Gun.cpp

---
+++ B. Han Solo and Lazer Gun.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <stack>
+#include <iomanip>
+using namespace std;
+
+#define ft first
+#define sd second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ll long long int
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define pii pair<int,int>
+#define plii pair<pair<ll, int>, int>
+#define piii pair<pii, int>
+#define viii vector<pair<pii, int>>
+#define vl vector<ll>
+#define vll vector<pair<ll,ll>>
+#define pll pair<ll,ll>
+#define pli pair<ll,int>
+#define mp make_pair
+#define ms(x, v) memset(x, v, sizeof x)
+const int mod = 1e9 + 7;
+const int maxn = 1e5 + 5;
+const int N=100005;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+    #endif
+    double n, x, y;
+    cin >> n >> x >> y;
+    map<double, int> mp;
+    int f = 0;
+    for (int i = 0; i < n; i++) {
+        double p, q;
+        cin >> p >> q;
+        if (p != x) {
+            mp[(q = y) / (p - x)]++;
+        }
+        else f = 1;
+    }
+    cout << mp.size() + f;
+   return 0;
+}
