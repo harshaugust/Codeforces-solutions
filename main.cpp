1f408d2a6d408f071ed89d49dc33c1b5fe7533e1
HARSH KUMAR <harsh@HARSHs-MacBook-Air.local>
2022-06-10 15:24:30 +0530

    added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 998244353
+#define int long long
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define vi vector<int>
+#define pii pair<int, int>
+const int N = 1e5 + 11;
+/*
+ */
+void solve()
+{
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+
+    // #endif
+    int t;
+    cin >> t;
+    // t = 1;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+    return 0;
+}
